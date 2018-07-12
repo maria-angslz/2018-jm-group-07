0275a3d9aa832f81d7b4ccae74961defe8f50a2b
orlandoleonardo <leonardoorlando9@gmail.com>
2018-07-12 10:28:30 -0300

    cambios durante la correccion

--- src/main/java/sge/Suministro/ZonaGeografica.java
+++ src/main/java/sge/Suministro/ZonaGeografica.java
@@ -13,7 +13,6 @@ public class ZonaGeografica {
 	List<Transformador> transformadores;
 	int radioCubierto;
 	Coordenates coordenadaCentral;
-	Comparator<Double> comparadorMinimo;
 	
 	public ZonaGeografica(List <Transformador> unTransformador,Coordenates centro ,int radio) {
 		transformadores = unTransformador;
@@ -28,13 +27,16 @@ public class ZonaGeografica {
 	
 	public void asignarTransformadorAcliente(Cliente cliente, Coordenates coord) {
 		transformadores.stream().min((tranf1,tranf2) -> Double.compare(tranf1.distanciaA(coord), tranf2.distanciaA(coord))).get().setCliente(cliente);
-		
 	    
 	}
 	
 	public Boolean pertenece(Coordenates coordenada) {
-		return Math.sqrt( Math.pow(coordenadaCentral.X() - coordenada.X(), 2)  + Math.pow(coordenadaCentral.Y() - coordenada.Y(),2)) <= radioCubierto;
+		return getDistancia(coordenada) <= radioCubierto;
 		
+	}
+
+	private double getDistancia(Coordenates coordenada) {
+		return Math.sqrt( Math.pow(coordenadaCentral.X() - coordenada.X(), 2)  + Math.pow(coordenadaCentral.Y() - coordenada.Y(),2));
 	}
 	
 }
