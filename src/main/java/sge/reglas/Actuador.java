f3bdb9eecee8eb22e08f3c520c90e0b8c88c2aea
orlandoleonardo <lorlando@est.frba.utn.edu.ar>
2018-09-04 21:55:42 -0300

    quito código comentado innecesario

--- src/main/java/sge/reglas/Actuador.java
+++ src/main/java/sge/reglas/Actuador.java
@@ -7,34 +7,21 @@ import sge.dispositivos.inteligentes.DispositivoInteligente;
 
 public class Actuador {
 	private String nombre;
-	//private Decodificador unDeco;
-//	private List<DispositivoInteligente> dispositivos;
-//	private DispositivoInteligente dispositivo;
+	// private Decodificador unDeco;
 	private Consumer<DispositivoInteligente> funcion;
-	
-//	public Actuador(String nombre, List<DispositivoInteligente> dispositivos, Consumer<DispositivoInteligente> unaFuncion  /*,Decodificador unDeco,*/) {
-//		this.nombre = nombre;
-//		//this.unDeco = unDeco;
-//		this.funcion = unaFuncion;
-//		this.dispositivos = dispositivos;
-//	}
-//	
-//	public Actuador(String nombre, DispositivoInteligente dispositivo, Consumer<DispositivoInteligente> unaFuncion) {
-//		this.nombre = nombre;
-//		this.funcion = unaFuncion;
-//		this.dispositivo = dispositivo;
-//	}
-	
+
 	public Actuador(String nombre, Consumer<DispositivoInteligente> unaFuncion) {
 		this.nombre = nombre;
 		this.funcion = unaFuncion;
 	}
-	
-	public void accionar(DispositivoInteligente dispositivo) { //idea: recibir lista de dispositivos por parametro
+
+	public void accionar(DispositivoInteligente dispositivo) { 
 //			dispositivos.forEach(unDispo->funcion.accept(unDispo));				
 		funcion.accept(dispositivo);
-		//unDeco.decodificarMensaje(unDispositivo.getIDfabrica()); ----> habria que decodificar el mensaje para este id de fabricante que ya se lo envio por parametro,
-		
+		// unDeco.decodificarMensaje(unDispositivo.getIDfabrica()); ----> habria que
+		// decodificar el mensaje para este id de fabricante que ya se lo envio por
+		// parametro,
+
 	}
-	
+
 }
