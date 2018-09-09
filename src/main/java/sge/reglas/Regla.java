396d6e212ea9c99ee788306086580421be407b2b
orlandoleonardo <leonardoorlando9@gmail.com>
2018-09-09 18:15:39 -0300

    sensor como clase abstracta, regla sin terminar

--- src/main/java/sge/reglas/Regla.java
+++ src/main/java/sge/reglas/Regla.java
@@ -3,17 +3,28 @@ package sge.reglas;
 import java.util.List;
 import java.util.function.Function;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 import sge.dispositivos.inteligentes.DispositivoInteligente;
 
 //import java.util.List;
 
+@Entity
 public class Regla {
+	
+	@Id @GeneratedValue
+	private long id;
+	
 	private String nombre;
 	//private List<Sensor> sensoresADisposicion; //pensamos que seria en un futuro una lista de sensores y actuadores, pero por el momenton por falta de informacion al respecto, lo planteamos para uno solo.
 	//private List<Actuador> actuadores;
-	private Sensor sensorADisposicion;
-	private Actuador actuador;
-	private Function<Float,Boolean> funcion;
+	private Sensor sensorADisposicion;//ver relacion
+	private Actuador actuador; //ver relacion
+	
+	private Function<Float,Boolean> funcion; //ver como persistir funciones
 	
 	
 	public Regla(String nombre,Sensor sensor, Actuador actuador) {
--- src/main/java/sge/reglas/Sensor.java
+++ src/main/java/sge/reglas/Sensor.java
@@ -1,8 +1,17 @@
 package sge.reglas;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-public interface Sensor {
+@Entity
+public abstract class Sensor {
 	
-	public float medir();
+	@Id @GeneratedValue
+	long id;
+	
+	public float medir() {
+		return -1; //valor negativo para reflejar que no hay implementación correcta aún
+	};
 	
 }
