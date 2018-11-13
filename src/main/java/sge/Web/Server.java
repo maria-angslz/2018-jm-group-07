aa1c5a4323ae95e2fc4270ad74efe900343b3299
maria-angslz <maria.angslz@gmail.com>
2018-11-13 16:18:13 -0300

    deploy

--- src/main/java/sge/Web/Server.java
+++ src/main/java/sge/Web/Server.java
@@ -4,7 +4,15 @@ import spark.Spark;
 
 public class Server {	
 	public void iniciar() {
-		Spark.port(9000);
+		Spark.port(getHerokuAssignedPort());
+		//Spark.port(9000);
 		Router.configure();
 	}
+	static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
