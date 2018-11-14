f775f8fad137b7b0aa69a8e741e9674b09c52aa2
leandroventura <ventura.leandro@hotmail.com>
2018-11-14 12:05:17 -0300

    Heroku commit

--- src/main/java/sge/Web/Server.java
+++ src/main/java/sge/Web/Server.java
@@ -4,7 +4,17 @@ import spark.Spark;
 
 public class Server {	
 	public void iniciar() {
-		Spark.port(9000);
+		//Spark.port(9000);
+		Spark.port(getHerokuAssignedPort());
+				
 		Router.configure();
 	}
+	
+	int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
